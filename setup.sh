75d551a01a810e9c5b37f8623831405e690d0a0d
Tim Collins <tim@thecollins.team>
2023-03-07 14:25:38 +0000

    chore: Set up initial bootstrap

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+k3d cluster create --config bootstrap/k3d.conf
+
+# Prevent users from accidentally deploying to the wrong cluster.
+currentContext=$(kubectl config current-context)
+if [ "$currentContext" == "k3d-large-workflows" ]; then
+    echo "Starting deployment to cluster..."
+else
+    echo "The kubectl context is not what we expected. Exiting for safety. Perhaps the k3d cluster failed to create?"
+    exit 1
+fi
+
+# Deploy Argo CD, which in turn deploys everything else
+kubectl apply -k bootstrap/argocd
+kubectl -n argocd rollout status statefulset/argocd-application-controller
+kubectl -n argocd rollout status deployment/argocd-repo-server
+kubectl -n argocd apply -f bootstrap/app-of-apps
+
+# Wait for Argo CD to start syncing its new-found applications
+sleep 30
+kubectl -n argo rollout status deployment/workflow-controller
+kubectl -n argo rollout status deployment/argo-server
+
+echo "Complete."
