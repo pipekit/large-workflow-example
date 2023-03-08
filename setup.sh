56a989dc82f0aae21a70f9b98e720cbb36e28ed2
Tim Collins <tim@thecollins.team>
2023-03-08 12:13:16 +0000

    remove minio volumemount

--- setup.sh
+++ setup.sh
@@ -23,4 +23,7 @@ kubectl -n argo rollout status deployment/workflow-controller
 kubectl -n argo rollout status deployment/argo-server
 kubectl -n argo-events rollout status deployment/controller-manager
 kubectl -n argo-events rollout status deployment/events-webhook
+kubectl -n ingress-nginx rollout status deployment/nginx-ingress-nginx-controller
+kubectl -n ingress-nginx rollout status daemonset/svclb-nginx-ingress-nginx-controller
+
 echo "Complete."
